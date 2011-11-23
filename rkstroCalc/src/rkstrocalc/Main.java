a3d8ed8d65bdb1b8839acebb2fa01b76a8724d77
Rodrigo Castro <r.kstro15@gmail.com>
2011-11-23 17:13:22 -0300

    Primer codigo de la calculadora en java, no funcional

---
+++ rkstroCalc/src/rkstrocalc/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rkstrocalc;
+
+/**
+ *
+ * @author rocastro
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Window.main(new String[0]);
+    }
+
+}
